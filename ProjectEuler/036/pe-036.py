15c378364e24dce3aaffa6f35a1fb15225113a9f
Ryan <obviouslyghosts@gmail.com>
2020-01-12 09:50:14 -0500

    the solution, needs validation

--- ProjectEuler/036/pe-036.py
+++ ProjectEuler/036/pe-036.py
@@ -8,10 +8,25 @@ in base 10 and base 2.
 leading zeros.)
 """
 
-limit = 100
+# theyre always odd, because even will end with a 0 in binary
 
-for i in range(limit):
-    # binary
-    b = bin(i)[2:]
-    print(b, b[::-1])
-    
+limit = 100
+answers = set()
+
+def isPalindromic( f ):
+    f = str(f)
+    if f[0] == "0": return False
+    if f == f[::-1]: return True
+    return False
+
+for i in range(1, limit, 2):
+    if isPalindromic(i):
+        b = bin(i)[2:]
+        if isPalindromic(b):
+            answers.add(i)
+##    
+##    # binary
+##    b = bin(i)[2:]
+##    print(b, b[::-1])
+
+print(answers)
