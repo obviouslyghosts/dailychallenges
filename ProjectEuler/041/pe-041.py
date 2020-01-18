bf3666bb6ef39ff44642fb1ca8f076a1c6d66208
Ryan Surface <obviouslyghosts@gmail.com>
2020-01-18 16:27:14 -0500

    working with itertools, non functioning

--- ProjectEuler/041/pe-041.py
+++ ProjectEuler/041/pe-041.py
@@ -5,3 +5,42 @@ is also prime.
 
 What is the largest n-digit pandigital prime that exists?
 """
+import itertools
+
+largest = "987654321"
+n = list(itertools.permutations(largest, len(largest)))
+digits = 10
+
+def checkPanDigital ( v, b ):
+    s = str(v)
+    if len(s) < b-1: return False
+    if len(s) > b-1 : return False
+    for i in range(1,b):
+        if not (len([x for x in s if x == str(i)]) == 1):
+            return False
+    return True
+
+def primeCheck(n):
+    n = int(n)
+    if n == 1: return False
+    if n == 2: return True
+    ## even check
+    if n%2<1: return False
+    ## only check below square root!
+    l = int(n**0.5)+1
+    for i in range(3,l,2):
+        if n%i <1: return False
+    return True
+
+def panDigitalPrime(limit):
+    upperLimit = int( str(limit)[::-1])
+    for i in n:
+        i = int("".join(k for k in i))
+        if primeCheck(i):
+            print("FOUND!")
+            return i
+    return 0
+
+print("searching for pandigital Primes...")
+answer = panDigitalPrime(largest)
+print(answer)
