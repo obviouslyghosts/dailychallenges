04c77df3996156966a9b696ef742aa6820f9fe9d
Ryan <obviouslyghosts@gmail.com>
2020-02-09 22:21:06 -0500

    trivial change

--- ProjectEuler/048/pe-048.py
+++ ProjectEuler/048/pe-048.py
@@ -4,10 +4,11 @@ The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
 Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
 """
 
+upper = 10
 
 ans = 0
 
-for i in range(1,11):
+for i in range(1,upper+1):
     ans += (i**i)
 
 print(ans)
