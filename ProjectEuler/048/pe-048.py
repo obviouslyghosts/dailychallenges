4ef1211ff32122e704772af14834c83e4b478ee1
Ryan <obviouslyghosts@gmail.com>
2020-02-10 22:24:57 -0500

    trim to significant digits

--- ProjectEuler/048/pe-048.py
+++ ProjectEuler/048/pe-048.py
@@ -4,11 +4,17 @@ The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
 Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
 """
 
-upper = 10
-
+limit = 10
+upper = int('9'*10)
 ans = 0
 
-for i in range(1,upper+1):
-    ans += (i**i)
+for i in range(1,limit+1):
+    a = str(i**i)
+    print(a)
+    a = int(a[len(a)-limit:])
+    print(a)
+    ans += a
+    ans = str(ans)
+    ans = int(ans[len(ans)-limit:])
 
 print(ans)
